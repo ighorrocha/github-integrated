bf643885bce3870e25f00f954baf6c864e3c7f9e
Ighor Da Rocha Miranda <google-oauth2|117832478618339350195>
2022-08-16 18:42:22 +0000

    test: new table

---
+++ definitions/setup.js
@@ -0,0 +1,10 @@
+// Read more in the JS API documentation here: https://docs.dataform.co/guides/javascript/js-api
+
+// publish("new_table")
+//   .type("table")
+//   .query(ctx => `select * from ${ctx.ref("source_table")}`);
+
+
+ publish("new_table")
+   .type("table")
+   .query(ctx => `select * from ${ctx.ref("source_table")}`);
